7686c7dd496c7e4733d3889d73fb8c8fb15a9912
mksipe <masonsipe0513@gmail.com>
2021-09-13 13:48:32 -0500

    fixed some housekeeping stuff

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,12 @@ extern crate clap;
 use clap::App;
 
 fn main() {
-    // The YAML file is found relative to the current file, similar to how modules are found
     let yaml = load_yaml!("cli.yml");
     let matches = App::from_yaml(yaml).get_matches();
     let anothermatch = App::from_yaml(yaml).get_matches();
+
     match matches {
-        ciphertext  => to_ascii(anothermatch.value_of("ciphertext").unwrap()),
-        _           => println!("{}","No matches found."),
+        _ciphertext     => to_ascii(anothermatch.value_of("ciphertext").unwrap()),
     }
 }
 fn to_ascii(input:&str){
